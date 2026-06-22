1021a53187388679e6b0ea44496be0768fe15d84
Jerbesson Silva <jerbessonc@gmail.com>
2026-06-22 09:52:43 -0300

    Standardize narrative tone across chapters in copy.json and update build

---
+++ docs/_app/immutable/chunks/B574KNrl.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1lrwaca)==null?void 0:s.base)??"/site-brega-funk";var a;const t=((a=globalThis.__sveltekit_1lrwaca)==null?void 0:a.assets)??e??"";export{t as a,e as b};
--- docs/_app/immutable/entry/start.BbE5f126.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/DlT7RI1B.js";export{o as load_css,r as start};
