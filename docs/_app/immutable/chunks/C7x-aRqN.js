34dc42beab1ad97f8645af9b4ce8ccb48466afa1
Jerbesson Silva <jerbessonc@gmail.com>
2026-06-23 00:51:01 -0300

    update github pages

--- docs/_app/immutable/chunks/C7x-aRqN.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_7a3pfd)==null?void 0:s.base)??"/site-brega-funk";var a;const t=((a=globalThis.__sveltekit_7a3pfd)==null?void 0:a.assets)??e??"";export{t as a,e as b};
