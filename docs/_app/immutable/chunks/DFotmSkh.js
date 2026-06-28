a93439af8507ff1f4f86af6534ca537c919a6ab4
Jerbesson Silva <jerbessonc@gmail.com>
2026-06-28 12:42:34 -0300

    update github pages

--- docs/_app/immutable/chunks/DFotmSkh.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1y1ueb6)==null?void 0:s.base)??"/site-brega-funk";var e;const t=((e=globalThis.__sveltekit_1y1ueb6)==null?void 0:e.assets)??a??"";export{t as a,a as b};
