c6d95323f8a96033741b7bddc0befb61baacdbf5
Jerbesson Silva <jerbessonc@gmail.com>
2026-07-02 09:24:14 -0300

    update github pages

---
+++ docs/_app/immutable/chunks/_GsV2gK0.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_dsm933)==null?void 0:s.base)??"/site-brega-funk";var e;const t=((e=globalThis.__sveltekit_dsm933)==null?void 0:e.assets)??a??"";export{t as a,a as b};
